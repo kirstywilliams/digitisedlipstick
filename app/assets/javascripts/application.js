124ddb70d5109c39a3c3c5f51345a462d9d72ed9
Kirsty Williams <kirsty@digitisedlipstick.com>
2015-03-20 00:49:22 +0000

    Logo animation base added.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require modernizr
+//= require libs/jquery.fullPage.js
+//= require libs/jquery.slimscroll.js
+//= require modules/app.js
+//= require libs/vivus.min.js
+
+$(function(){
+	App.init();
+})
