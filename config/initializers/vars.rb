cd99758ff448a89bb3261e098add1bdef3aa711d
Kirsty Williams <kirsty@digitisedlipstick.com>
2015-03-21 04:29:56 +0000

    Added heroku prod gem.

---
+++ config/initializers/vars.rb
@@ -0,0 +1,14 @@
+SITE_BASE_TITLE = 'Digitised Lipstick | Web Design and Development'
+SITE_KEYWORDS = 'Web Development, Social Media Marketing, Email Marketing, Freelance Web Designer, Web Design Consultant'
+SITE_DESCRIPTION = 'Digitised Lipstick is a full service Website Design and Development company. Through Digitised Lipstick I deliver professional Web Design, SEO and Social Media Marketing services to small and medium sized businesses.'
+SITE_DOMAIN = 'www.digitisedlipstick.com'
+
+SITE_GOOGLE_ANALYTICS = 'UA-60714663-1'
+
+TWITTER_NAME = 'digitisedlipstk'
+GOOGLE_PLUS_NAME = 'kirstywilliams2013'
+FACEBOOK_NAME = 'digitisedlipstick'
+
+APP_NAME = 'Digitised Lipstick'
+APP_SLOGAN = 'Look. Feel. Work.'
+BRAND_COLOR = '#ED0F5A'
